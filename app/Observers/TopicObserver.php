a03bf9e53c4ff8511da87dba3a72c8373655e9d1
kitty.cheng <450038893@qq.com>
2020-09-13 20:25:11 +0800

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,7 +2,7 @@
 
 namespace App\Observers;
 
-use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -26,10 +26,16 @@ class TopicObserver
         $topic->body = clean($topic->body, 'user_topic_body');
         // 截取body 的内容，作为excerpt的内容
         $topic->excerpt = make_excerpt($topic->body);
+    }
 
+    public function saved(Topic $topic)
+    {
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if (!$topic->slug) {
-            $topic->slug =  app(SlugTranslateHandler::class)->translate($topic->title);
+            // $topic->slug =  app(SlugTranslateHandler::class)->translate($topic->title);
+
+            // 推送任务到队列，实现异步处理
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
