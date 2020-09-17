fc1f2c44e526872297df45134d8c3d14dc06d7dd
kitty.cheng <450038893@qq.com>
2020-09-17 23:39:28 +0800

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -23,13 +23,26 @@ $api = app(\Dingo\Api\Routing\Router::class);
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function($api) {
-    // 发送验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
 
-    // 用户注册
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
+    //访问频率控制分组
+    $api->group([
+        // 访问频率中间件
+        // 1分钟调用1次
+        'middleware' => 'api.throttle',
+        // 调用次数
+        'limit' => config('api.rate_limits.sign.limit'),
+        // 周期 单位：分钟
+        'expires' => config('api.rate_limits.sign.expires'),
+    ],function($api){
+        // 发送验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+
+        // 用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+    });
+
 
 });
 
